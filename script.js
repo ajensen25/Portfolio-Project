0d1617bfe9f771edba4409ab33ce0ea57b45e749
ajensen25 <aidenjensen707@gmail.com>
2025-03-05 17:35:22 -0500

    added dark mode toggling

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ menuButton.addEventListener('click', () => {
 closeMenuButton.addEventListener('click', () => {
   menuButton.click();
 });
+
+
+// Adding dark mode toggle functionality
+
+const darkModeButton = document.querySelector('.dark-mode-button');
+const root = document.querySelector(':root');
+const body = document.querySelector('body');
+
+darkModeButton.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')) {
+    toggleDarkTheme();
+  }
+  else {
+    toggleLightTheme();
+  }
+})
+
+function toggleLightTheme() {
+  root.style.setProperty('--header-color', 'rgba(255, 255, 255, 0.3)');
+  root.style.setProperty('--white-color', '#fff');
+  root.style.setProperty('--black-color', '#000');
+  root.style.setProperty('--white-color-thin', '#ffffff7f');
+  root.style.setProperty('--black-color-thin', '#0000007b');
+}
+
+function toggleDarkTheme() {
+  root.style.setProperty('--header-color', 'rgba(50, 50, 50, 0.3)');
+  root.style.setProperty('--white-color', 'rgb(50, 50, 50)');
+  root.style.setProperty('--black-color', '#fff');
+  root.style.setProperty('--white-color-thin', 'rgba(50, 50, 50, 0.5)');
+  root.style.setProperty('--black-color-thin', 'rgba(255, 255, 255, 0.5)');
+}
